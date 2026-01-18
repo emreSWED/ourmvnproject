1720b5b1a065c4390e44033d3de7b4d8eb307d62
emreSWED <emre.oezoener@stud.fra-uas.de>
2026-01-18 20:20:33 +0100

    Added random route generation for vehicles

--- src/main/java/loader/VehicleAdder.java
+++ src/main/java/loader/VehicleAdder.java
@@ -5,18 +5,18 @@ import de.tudresden.sumo.cmd.Vehicle;
 import model.MyVehicle;
 import util.ConnectionManager;
 
-
+import static loader.RouteGenerator2.routeidcounter;
 
 
 public class VehicleAdder {
     public static int vehCounter = 0;
     public static ConnectionManager conn;
     public VehicleAdder() {
-
     }
     public static void addRandomVehicle() throws Exception {
-        int randomRouteVariable = (int)(Math.random()*5);
-        conn.dojobset(Vehicle.add("ourVehicle"+vehCounter,"DEFAULT_VEHTYPE", "route"+randomRouteVariable, (int)conn.dojobget(Simulation.getCurrentTime())+1, 0.0,15.0, (byte) 0));
+        //int randomRouteVariable = (int)(Math.random()*5);
+        RouteGenerator2 routeGenerator = new RouteGenerator2();
+        conn.dojobset(Vehicle.add("ourVehicle"+vehCounter,"DEFAULT_VEHTYPE", "route"+(routeidcounter-1), (int)conn.dojobget(Simulation.getCurrentTime())+1, 0.0,15.0, (byte) 0));
         vehCounter++;
     }
 }
