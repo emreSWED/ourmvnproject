8ad7ba2945488b82e814ff4ab5a597f60e64525f
emreSWED <emre.oezoener@stud.fra-uas.de>
2026-01-18 17:55:41 +0100

    v2.1 of splitting tl clusters into single trafficlights.

--- src/main/java/loader/LaneTrafficLight.java
+++ src/main/java/loader/LaneTrafficLight.java
@@ -1,5 +1,6 @@
 package loader;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,8 @@ public class LaneTrafficLight {
     public Double xpos;
     public Double ypos;
     public List<SingleTrafficLight> singleTrafficLights = new ArrayList<SingleTrafficLight>();
+    public Shape shape;
+    public char state;
 
     public LaneTrafficLight(List<SingleTrafficLight> singleTrafficLights) {
         this.singleTrafficLights.addAll(singleTrafficLights);
@@ -17,23 +20,27 @@ public class LaneTrafficLight {
         this.fromLane = singleTrafficLights.getFirst().fromLane;
         this.xpos = singleTrafficLights.getFirst().xpos;
         this.ypos = singleTrafficLights.getFirst().ypos;
+        this.state = singleTrafficLights.getFirst().clusterState.charAt(singleTrafficLights.getFirst().index);
     }
 
     public void setGreen(){
         for(SingleTrafficLight singleTrafficLight : singleTrafficLights){
             singleTrafficLight.setGreenNonPriority();
         }
+        state = 'g';
     }
 
     public void setYellow(){
         for(SingleTrafficLight singleTrafficLight : singleTrafficLights){
             singleTrafficLight.setYellow();
         }
+        state = 'y';
     }
 
     public void setRed(){
         for(SingleTrafficLight singleTrafficLight : singleTrafficLights){
             singleTrafficLight.setRed();
         }
+        state = 'r';
     }
 }
